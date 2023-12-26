4cef585cca120fb91c41d31b8a558de3a3ae676d
Junyan Jiang <201499832@qq.com>
2023-12-27 00:45:07 +0800

    2023.12.27

--- src/Edgedetect.v
+++ src/Edgedetect.v
@@ -1,24 +1,22 @@
-//////////////////////////////////////////////////////////////////////////////////
-module Edgedetect(
-    input key,      // 按钮输入
-    input clk,      // 时钟信号
-    output pulse // 脉冲输出
+module Edgedetect
+(
+    input   key,    // 按钮输入
+    input   clk,    // 时钟信号
+    output  pulse   // 脉冲输出
 );
 
-reg key_prev; // 存储前一个按键状态
-reg pulse_reg; // always块中储存状态
+    reg     key_prev;   // 存储前一个按键状态
+    reg     pulse_reg;  // always块中储存状态
 
-// 在每个时钟上升沿更新按键状态
-always @(posedge clk) begin
-    key_prev <= key;
-    // 当检测到按键的负沿时，生成脉冲
-    if (key_prev & ~key) 
-        pulse_reg <= 1;
-    else 
-        pulse_reg <= 0;
-end
 
-assign pulse = pulse_reg;
+    always @(posedge clk) begin
+        key_prev <= key;
+        if (key_prev & ~key) 
+            pulse_reg <= 0;
+        else 
+            pulse_reg <= 1;
+    end
+
+    assign pulse = pulse_reg;
 
 endmodule
-//////////////////////////////////////////////////////////////////////////////////
--- src/KillShake.v
+++ src/KillShake.v
@@ -1,31 +1,29 @@
-//////////////////////////////////////////////////////////////////////////////////
-module Killshake(
-    input clk,        // 时钟信号
-    input key,  // 含噪声的按键输入
-    output signal  // 清洁的按键输出
+module Killshake
+(
+    input   clk,    // 时钟信号
+    input   key,    // 含噪声的按键输入
+    output  signal  // 清洁的按键输出
 );
 
-parameter DEBOUNCE_TIME = 1000000; // 去抖时间阈值，根据时钟频率调整,1/50s
-reg [19:0] count;       // 计数器，位宽取决于DEBOUNCE_TIME
-reg key_state;          // 存储稳定后的按键状态
-reg signal_reg;         // always块中储存状态
+    parameter   DEBOUNCE_TIME = 1000000;    // 去抖时间阈值，根据时钟频率调整,1/50s
+    reg [19:0]  count;                      // 计数器，位宽取决于DEBOUNCE_TIME
+    reg         key_state;                  // 存储稳定后的按键状态
+    reg         signal_reg;                 // always块中储存状态
 
-always @(posedge clk) begin
-    if (key == key_state) begin
-        // 如果当前输入状态与去抖后的状态相同，则增加计数器
-        if (count < DEBOUNCE_TIME) 
-            count <= count + 1;
-        else
-            signal_reg <= key_state; // 更新输出状态
-    end else begin
-        // 如果输入状态改变，重置计数器并更新去抖后的状态
-        count <= 0;
-        key_state <= key;
+    always @(posedge clk) begin
+        if (key == key_state) begin
+            // 如果当前输入状态与去抖后的状态相同，则增加计数器
+            if (count < DEBOUNCE_TIME) 
+                count <= count + 1;
+            else
+                signal_reg <= key_state; // 更新输出状态
+        end else begin
+            // 如果输入状态改变，重置计数器并更新去抖后的状态
+            count <= 0;
+            key_state <= key;
+        end
     end
-end
 
-assign signal = signal_reg;
+    assign signal = signal_reg;
 
 endmodule
-
-//////////////////////////////////////////////////////////////////////////////////
